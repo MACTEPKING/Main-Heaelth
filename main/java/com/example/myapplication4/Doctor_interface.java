f8bf36bbe211769ba30066361969dfdbc0bbaeac
MACTEPKING <103208765+MACTEPKING@users.noreply.github.com>
2022-09-04 17:22:23 +0300

    Update Doctor_interface.java

--- main/java/com/example/myapplication4/Doctor_interface.java
+++ main/java/com/example/myapplication4/Doctor_interface.java
@@ -27,38 +27,5 @@ public class Doctor_interface extends AppCompatActivity {
         give_consultation = findViewById(R.id.give_consultation);
         no_give_consultation = findViewById(R.id.no_give_consultation);
 
-        try {
-            Socket clientSocket = new Socket();
-            clientSocket.connect(new InetSocketAddress("localhost", 7777));
-            Scanner in = new Scanner(clientSocket.getInputStream());
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            while (true){
-                if (in.hasNextLine()){
-                    String response = in.nextLine();
-                    give_consultation.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            out.write("accept " + response);
-                        }
-                    });
-                    no_give_consultation.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            out.write("do_not_accept " + response);
-                        }
-                    });
-                }
-                change_data.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(Doctor_interface.this, Doctor_data.class);
-                        startActivity(intent);
-                    }
-                });
-            }
-        } catch (IOException e) {
-            System.err.println(e);
-        }
     }
 }
